d21976d32482198bc0073805b1f9403e2247cbca
justacho936 <justoejc@gmail.com>
2022-12-07 06:33:50 -0300

    vigesimoseptimo ejercicio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -195,7 +195,8 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  var combinado = nombre + " " + apellido;
+  return combinado;
 }
 
 function obtenerSaludo(nombre) {
