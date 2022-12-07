deaed6cb052d13a8818a39d9c0cdee809cb3f5bd
justacho936 <justoejc@gmail.com>
2022-12-06 23:35:16 -0300

    vigesimo primer ejercicio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -147,7 +147,7 @@ function elevarAlCubo(num) {
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  
+  return Math.pow(num, exponent);
 }
 
 function redondearNumero(num) {
