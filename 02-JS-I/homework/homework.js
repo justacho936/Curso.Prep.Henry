f6b97eb446401d2cabd396d76e9705fdc2b3ec0d
justacho936 <justoejc@gmail.com>
2022-12-06 21:26:05 -0300

    tercer ejercicio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "Hola";
 const nuevoNum = 51;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
