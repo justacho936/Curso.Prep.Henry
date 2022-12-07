6b3fae78baa298a254368943b554bbc56d851878
justacho936 <justoejc@gmail.com>
2022-12-07 06:41:33 -0300

    vigesimooctavo ejercicio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -203,7 +203,8 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  var concatenar = "Hola" + " " + nombre + "!";
+  return concatenar;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
