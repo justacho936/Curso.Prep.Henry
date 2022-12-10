e45a9c82593f554589ba5fd47d15a450a8ac212f
justacho936 <justoejc@gmail.com>
2022-12-10 20:38:13 -0300

    trigesimo ejercicio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -218,7 +218,8 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  var cuatroLadosIguales = lado * 4;
+  return cuatroLadosIguales;
 }
 
 
