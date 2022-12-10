6ab9fcea34a608ca8bbc409e1a189c12226fa1ba
justacho936 <justoejc@gmail.com>
2022-12-10 20:30:01 -0300

    vigesimonoveno ejercicio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -210,7 +210,8 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+ var area = alto * ancho;
+ return area; 
 }
 
 
