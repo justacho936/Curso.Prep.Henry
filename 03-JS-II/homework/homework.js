55e6d329c9c857df49ab5802487a1a10ad005be4
justacho936 <justoejc@gmail.com>
2022-12-12 17:35:56 -0300

    ejercicio nueve

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -116,6 +116,10 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 15 === 0) return "fizzBuzz";
+  if (numero % 5 === 0) return "buzz";
+  if (numero % 3 === 0) return "fizz";
+  return numero;
 }
 
 function operadoresLogicos(num1, num2, num3) {
