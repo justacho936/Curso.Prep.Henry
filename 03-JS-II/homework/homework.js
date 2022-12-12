27ba7cd0d39190848fae0352475f29c251cd2657
justacho936 <justoejc@gmail.com>
2022-12-12 19:01:47 -0300

    ejercicio trece

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -175,8 +175,13 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
-}
+  let arrayTablaDelSeis[]
+    for (let i = 0; i < 11; i++) {
+      arrayTablaDelSeis.push(6 * i)
+    }
+    return arrayTablaDelSeis;
+   }
+
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
