9b6ea68afd6801f08296109c1aef85a506bea188
justacho936 <justoejc@gmail.com>
2022-12-11 22:52:39 -0300

    ejercicio cinco

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -46,6 +46,15 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === "aleman") {
+    return "Guten Tag!";
+  } else if (idioma === "mandarin") {
+    return "Ni Hao!";
+  } else if (idioma === "ingles") {
+    return "Hello!";
+  }else{
+    return "Hola!";
+  }
 }
 
 function colors(color) {
