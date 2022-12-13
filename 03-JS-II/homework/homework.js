5e072c82f24222be13cf94fe95e362695aa8f43c
justacho936 <justoejc@gmail.com>
2022-12-12 22:12:48 -0300

    ejercicio quince

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -196,6 +196,15 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+
+  var a = numero;
+  var i = 0;
+
+  do {
+    a = a + 5;
+    i = i + 1;
+  } while (i < 8) 
+    return a;
 }
 
 
