58e6b73b98f3bb7720b2c15ae3ea5b1f7d8f71da
justacho936 <justoejc@gmail.com>
2022-12-14 19:23:59 -0300

    ejercicio 10

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -93,6 +93,7 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  return agregarNumeros(resultadosTest) / resultadosTest.length;
 }
 
 
