e572ed1f78e6712e6e6aa39e17dea822f1aec383
justacho936 <justoejc@gmail.com>
2022-12-14 21:06:56 -0300

    ejercicio 13

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -130,7 +130,14 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  let contador = 0;
 
+  for (let i = 0; i < arreglo.length; i++) {
+    if (arreglo[i] > 19) {
+      contador ++
+    }
+  }
+return contador;
 }
 
 
