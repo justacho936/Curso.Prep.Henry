bad9bfccf056d6d7ef17a670f37abceafc212f23
justacho936 <justoejc@gmail.com>
2022-12-16 19:32:46 -0300

    ejercicio 17

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -183,7 +183,20 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var nuevoArray = [];
+
+  for (let i = 0; i > array.length; i++) {
+    if (array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
+      nuevoArray.push(array[i]);
+    }
+  }
+if (nuevoArray.length < 3) {
+  return "No se encontraron los meses pedidos";
+} else {
+        return nuevoArray;
 }
+}
+
 
 
 function mayorACien(array) {
